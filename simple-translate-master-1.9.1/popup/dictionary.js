c02f4853b4b4a1e6b39a82095100cedef06ef91a
Yuan Shen <sawyuanshen@hotmail.com>
2020-07-11 01:25:09 +0800

    extension 3(simple-translate): 1. Added id for button in popup.html 2. Added paragraph in popup.html to display the definition 3. link dictionary.js to popup.html and add eventlistener for the search dictionary button 4. link required element into dictionary.js for query the api and done check isempty for the text area, added checking for the response to see whether got definition response or no definition

--- simple-translate-master-1.9.1/popup/dictionary.js
+++ simple-translate-master-1.9.1/popup/dictionary.js
@@ -1,29 +1,40 @@
 let searchResult = document.getElementById("searchResult");
-let meaning = document.getElementById("meaning");
+let buttondic = document.getElementById("buttondic");
+let keyword = document.getElementById("textarea");
 
-//create request code
-let request = new XMLHttpRequest()
+//popoutoptions
+buttondic.addEventListener("click",
+    function (event) {
+        if(!keyword.value.replace(/\s/g, '').length) { //check if the text area are empty or whitespace only
+            searchResult.innerHTML = "Definition: please enter a word"; //indicate word required
+            return
+        }
+            //create request code
+        let request = new XMLHttpRequest()
 
 //the keyword is the word that user wants to search for
-request.open('GET', 'https://dictionaryapi.com/api/v3/references/collegiate/json/'+keyword+'?key=f3ea7727-6af6-4dd1-ac4d-4278bca3863c')
+        request.open('GET', 'https://dictionaryapi.com/api/v3/references/collegiate/json/'+keyword.value+'?key=f3ea7727-6af6-4dd1-ac4d-4278bca3863c');
 
-request.send();
+        request.send();
 
-console.log("hello ");
-request.onload = () =>
-{
-    console.log(request);
-    if (request.status == 200) {
-        var data = JSON.parse(request.response);
-        console.log(request.response);
-        console.log(data[0].shortdef.toString());
-        console.log(data.responseURL );
-        searchResult.innerHTML = data[0].shortdef; //this 1 fetches the definition
+        request.onload = () =>
+        {
 
+            console.log(request);
+            if (request.status == 200) {
 
+                var data = JSON.parse(request.response);
+                if(data[0].shortdef.length < 1){//check there are definition from response
+                    searchResult.innerHTML = "Definition: no meaning found"; //return no result found found
+                }else{
+                    searchResult.innerHTML = "Defenition: " + data[0].shortdef;
+                } //this 1 fetches the definition
 
+            }
+            else { //if not found in dictionary
+                console.log(`error ${request.status} ${request.statusText}`)
+            }
+        }
     }
-    else { //if not found in dictionary
-        console.log(`error ${request.status} ${request.statusText}`)
-    }
-}
+);
+
